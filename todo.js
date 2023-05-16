ee6fefd19cdbb26669f08cf3f40fbd307ba2f5aa
Abhi <abhi3s6y9@gmail.com>
2023-05-16 11:17:13 +0530

    Create function to add task when enter key is pressed after filling the input field

--- todo.js
+++ todo.js
@@ -3,7 +3,6 @@ const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
 
-console.log('Working');
 
 function renderList () {}
 
@@ -11,6 +10,35 @@ function markTaskAsComplete (taskId) {}
 
 function deleteTask (taskId) {}
 
+// To add the Task in the tasks array and call renderList function after that
 function addTask (task) {}
 
-function showNotification(text) {}
+// Function to show Notification when there is some error, task added or deleted
+function showNotification(text) {
+    alert(text);
+}
+
+function handleInputKeypress(e) {
+    if(e.key == 'Enter'){
+        const text = e.target.value;
+
+        if (!text) {
+            showNotification('Task text can not be empty');
+            return;
+        }
+
+        // Task object
+        const task = {
+            text, // short hand for "text: text,"
+            id: Date.now().toString(),
+            done: false
+        }
+
+        e.target.value = '';
+        addTask(task);
+    }
+
+}
+
+addTaskInput.addEventListener('keyup', handleInputKeypress);
+
