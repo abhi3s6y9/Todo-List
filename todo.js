200fc68c997e5fd3a807ccb319eac204d394792b
Abhi <abhi3s6y9@gmail.com>
2023-05-17 00:45:24 +0530

    Create a function to delete Tasks

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-const tasks = [];
+let tasks = [];
 const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
@@ -8,10 +8,31 @@ function renderList () {}
 
 function markTaskAsComplete (taskId) {}
 
-function deleteTask (taskId) {}
+// To delete a task from array and show notification
+function deleteTask (taskId) {
+    // Create a new task array from the existing tasks array by using filter function of array
+    const newTasks = tasks.filter(function(task){
+        return task.id !== taskId
+    })
+
+    // make the original tasks array point to newTasks array
+    tasks = newTasks;
+
+    renderList();
+    showNotification("Task deleted successfully");
+}
 
 // To add the Task in the tasks array and call renderList function after that
-function addTask (task) {}
+function addTask (task) {
+    if(task){
+        tasks.push(task);
+        renderList();
+        showNotification("Task added successfully");
+        return;
+    }
+
+    showNotification("Task cannot be added");
+}
 
 // Function to show Notification when there is some error, task added or deleted
 function showNotification(text) {
